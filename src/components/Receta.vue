65031e9a2bcc5fb97e20e4c193f1fd6af378894f
piiivot <the_pivotsk8@hotmail.com>
2023-07-07 16:51:07 -0500

    feat:componente receta

---
+++ src/components/Receta.vue
@@ -0,0 +1,13 @@
+<script setup>
+defineProps({
+  receta: {
+    type: Object,
+  },
+});
+</script>
+
+<template>
+  <div>
+    <h2>{{ receta.strDrink }}</h2>
+  </div>
+</template>
--- src/views/InicioView.vue
+++ src/views/InicioView.vue
@@ -1,5 +1,13 @@
-<script setup></script>
+<script setup>
+import { useBebidasStore } from '../stores/bebidas';
+import Receta from '../components/Receta.vue';
+const bebidas = useBebidasStore();
+console.log(bebidas.receta);
+</script>
 
 <template>
   <h1 class="text-6xl font-extrabold">Recetas</h1>
+  <div class="grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 my-10 gap-10">
+    <Receta v-for="receta in bebidas.recetas" :receta="receta" :key="idDrink" />
+  </div>
 </template>
